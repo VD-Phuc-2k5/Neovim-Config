96d47ec0e410e8ed3254cb080e5fa949780bfea7
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-08-28 16:16:38 +0700

    feat: update formatting configuration

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,41 +1,47 @@
 -- formatting.lua
 return {
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        go = { "gofmt" },
-        cpp = { "clang_format" },
-        c = { "clang_format" },
-      },
-      -- Custom formatters
-      formatters = {
-        prettier = {
-          prepend_args = { "--single-quote", "--jsx-single-quote" },
-        },
-      },
-    },
-    config = function(_, opts)
-      require('conform').setup(opts)
-      -- Format command
-      vim.keymap.set('n', '<leader>f', function()
-        require('conform').format({ async = true, lsp_fallback = true })
-      end, { desc = 'Format buffer' })
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			notify_on_error = false,
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				go = { "gofmt" },
+				cpp = { "clang_format" },
+				c = { "clang_format" },
+			},
+			-- Custom formatters with 80 char line limit
+			formatters = {
+				prettier = {
+					prepend_args = {
+						"--single-quote",
+						"--jsx-single-quote",
+						"--print-width=80",
+					},
+				},
+				stylua = {
+					prepend_args = { "--column-width", "80" },
+				},
+				clang_format = {
+					prepend_args = { "--style={ColumnLimit: 80}" },
+				},
+			},
+		},
+		config = function(_, opts)
+			require("conform").setup(opts)
+			-- Manual format command only
+			vim.keymap.set("n", "<leader>f", function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end, { desc = "Format buffer manually" })
+		end,
+	},
 }
