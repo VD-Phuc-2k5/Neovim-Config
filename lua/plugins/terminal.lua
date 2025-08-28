06fcc6234b1121d223203a981823522d2ff85960
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-08-28 16:48:53 +0700

    refactor: use pwsh for floattern

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -16,7 +16,7 @@ return {
         persist_size = true,
         direction = "horizontal",
         close_on_exit = true,
-        shell = vim.o.shell,
+        shell = "powershell.exe",
         float_opts = {
           border = "curved",
           winblend = 0,
@@ -26,6 +26,53 @@ return {
           },
         },
       })
+
+      -- PowerShell Core terminal
+     local pwsh = Terminal:new({
+        cmd = "pwsh.exe",
+        dir = "git_dir", 
+        direction = "horizontal",
+        float_opts = {
+          border = "double",
+        },
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+        end,
+        on_close = function(term)
+          vim.cmd("startinsert!")
+        end,
+      })
+
+      local powershell_float = Terminal:new({
+        cmd = "powershell.exe",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          width = 120,
+          height = 30,
+        },
+        on_open = function(term)
+          vim.cmd("startinsert!")
+        end,
+      })
+
+      function _POWERSHELL_TOGGLE()
+        powershell:toggle()
+      end
+
+      function _PWSH_TOGGLE()
+        pwsh:toggle()
+      end
+
+      function _POWERSHELL_FLOAT_TOGGLE()
+        powershell_float:toggle()
+      end
+
+      vim.api.nvim_set_keymap("n", "<leader>tp", "<cmd>lua _POWERSHELL_TOGGLE()<CR>", {noremap = true, silent = true, desc = "Toggle PowerShell"})
+      vim.api.nvim_set_keymap("n", "<leader>tc", "<cmd>lua _PWSH_TOGGLE()<CR>", {noremap = true, silent = true, desc = "Toggle PowerShell Core"})
+      vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua _POWERSHELL_FLOAT_TOGGLE()<CR>", {noremap = true, silent = true, desc = "Toggle Floating PowerShell"})
     end,
   },
 }
