848b18082ef32cacf7c427fc2cb3242e041e6aa5
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-08-30 23:06:09 +0700

    feat: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,6 @@ vim.keymap.set(
   "<cmd>ToggleTerm<CR>",
   { desc = "Toggle Terminal" }
 )
+
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
